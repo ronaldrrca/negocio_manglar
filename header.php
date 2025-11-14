264ea7fd25dc6fcaf95bd959e79d694cba9fe896
Ronald Rodriguez Campillo <ronaldrrca@gmail.com>
2025-11-14 15:25:01 -0500

    agregando el header y el esquema de archivos y carpetas

---
+++ header.php
@@ -0,0 +1,10 @@
+<h1>Variedades Manglar</h1>
+<div class="header_info">
+    <span class="solo_domicilios">Sólo domicilios</span>
+    <span class="horario">Horario de atención:</span>
+    <div class="horario_info">
+        <span class="horario">Lunes a sábado: 8am - 7pm</span><span class="horario_slash">&nbsp / &nbsp</span>
+        <span class="horario">Domingos y festivos: 8am - 12pm</span>
+    </div>
+</div>
+    
